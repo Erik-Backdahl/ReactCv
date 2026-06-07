0b8635266d8f4061f48160bf4378f973f3393822
Erik Bäckdahl <erik.backdahl@gmail.com>
2026-06-07 22:16:56 +0200

    added base to fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ReactCv/',
   server: {
     // Ensures refreshing any route returns index.html (no 404s)
     historyApiFallback: true,
